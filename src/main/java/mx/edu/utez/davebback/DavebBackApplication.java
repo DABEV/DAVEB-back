7bdebc80a35d2a19dc02a56515c5c18918c44692
Gandy Esau Avila Estrada <20193tn128@utez.edu.mx>
2022-02-05 13:42:35 -0600

    Proyecto inicializado de Spring v2.6.3

---
+++ src/main/java/mx/edu/utez/davebback/DavebBackApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.davebback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavebBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavebBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/utez/davebback/DavebBackApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.davebback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavebBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
